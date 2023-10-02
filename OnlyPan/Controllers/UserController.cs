b6369b5d6acada65a4a6f68f8cb7b5f446dd5638
santorar <estractor179@gmail.com>
2023-10-02 10:56:56 -0500

    Changed the request method from SqlConection to EntityFramework

--- OnlyPan/Controllers/UserController.cs
+++ OnlyPan/Controllers/UserController.cs
@@ -1,9 +1,7 @@
-using System.Data;
 using System.Security.Claims;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
 using OnlyPan.Models;
 using OnlyPan.Models.ViewModels;
@@ -13,10 +11,12 @@ namespace OnlyPan.Controllers;
 public class UserController : Controller
 {
   private readonly OnlyPanContext _context;
+
   public UserController(OnlyPanContext context)
   {
     _context = context;
   }
+
   //Login Views and controller
   // TODO move the operation to the utilities folder
   public IActionResult Login()
@@ -26,9 +26,10 @@ public class UserController : Controller
     {
       if (c.Identity.IsAuthenticated)
       {
-        return RedirectToAction("Index","Home");
+        return RedirectToAction("Index", "Home");
       }
     }
+
     return View();
   }
 
@@ -37,58 +38,46 @@ public class UserController : Controller
   {
     try
     {
-      using (SqlConnection con = new(_context.Database.GetConnectionString()))
+      var userdb = _context.Usuarios.FromSqlRaw("EXECUTE sp_validate_user {0}, {1}", user.Correo, user.Contra).ToList()[0] ?? throw new SystemException();
+      List<Claim> c = new List<Claim>()
       {
-        using (SqlCommand cmd = new("sp_validate_user",con))
-        {
-          cmd.CommandType = CommandType.StoredProcedure;
-          cmd.Parameters.Add("@Email", SqlDbType.VarChar).Value = user.Correo;
-          cmd.Parameters.Add("@Password", SqlDbType.VarChar).Value = user.Contra;
-          con.Open();
-          var dr = cmd.ExecuteReader();
-          while (dr.Read())
-          {
-              List<Claim> c = new List<Claim>()
-              {
-                new Claim(ClaimTypes.Email, user.Correo),
-                new Claim(ClaimTypes.Name, dr[4].ToString() ?? throw new InvalidOperationException()),
-                new Claim(ClaimTypes.Role, dr[1].ToString() ?? throw new InvalidOperationException())
-              };
-              ClaimsIdentity ci = new(c, CookieAuthenticationDefaults.AuthenticationScheme);
-              AuthenticationProperties p = new AuthenticationProperties();
-              p.AllowRefresh = true;
-              p.IsPersistent = user.Remember;
-              
-              if (user.Remember)
-                p.ExpiresUtc = DateTimeOffset.UtcNow.AddHours(1);
-              else
-                p.ExpiresUtc = DateTimeOffset.MaxValue;
-              await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(ci), p);
-              return RedirectToAction("Index", "Home");
-          }
-          ViewBag.Error = "Datos incorrectos o cuenta no registrada.";
-          con.Close();
-        }
-      }
+        new Claim(ClaimTypes.Email, user.Correo),
+        new Claim(ClaimTypes.Name, userdb.Nombre),
+        new Claim(ClaimTypes.Role, userdb.Rol.ToString())
+      };
+      ClaimsIdentity ci = new(c, CookieAuthenticationDefaults.AuthenticationScheme);
+      AuthenticationProperties p = new AuthenticationProperties();
+      p.AllowRefresh = true;
+      p.IsPersistent = user.Remember;
+      
+      if (user.Remember)
+        p.ExpiresUtc = DateTimeOffset.UtcNow.AddHours(1);
+      else
+        p.ExpiresUtc = DateTimeOffset.MaxValue;
+      await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(ci), p);
+      return RedirectToAction("Index", "Home");
     }
     catch (SystemException e)
     {
-      ViewBag.Error = "Conexion Fallida";
+      ViewBag.Error = "Datos incorrectos o Usuario no registrado";
     }
+
     return View();
   }
-    
-  //Register Views
+
+//Register Views
   public IActionResult Register()
   {
     return View();
   }
-  //TODO make a way to encrypt the password of the user and his personal data directy in the register
+
+//TODO make a way to encrypt the password of the user and his personal data directy in the register
   [ValidateAntiForgeryToken]
   [HttpPost]
   public async Task<IActionResult> Register(RegisterViewModel model)
   {
-    if(ModelState.IsValid){
+    if (ModelState.IsValid)
+    {
       var user = new Usuario()
       {
         Nombre = model.Nombre,
@@ -98,10 +87,10 @@ public class UserController : Controller
       _context.Add(user);
       var result = await _context.SaveChangesAsync();
       {
-        
       }
       return RedirectToAction(nameof(Login));
     }
+
     return View(model);
   }
 
