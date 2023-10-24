707185fc31b4c5984b5675091a94d26997c4b4a2
santorar <estractor179@gmail.com>
2023-10-23 23:05:37 -0500

    Added some new models for the db

---
+++ OnlyPan/Models/ImagenRecetum.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OnlyPan.Models;
+
+public partial class ImagenRecetum
+{
+    public int IdImagen { get; set; }
+
+    public int IdReceta { get; set; }
+
+    public byte[]? Imagen { get; set; }
+}
---
+++ OnlyPan/Models/Unidad.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OnlyPan.Models;
+
+public partial class Unidad
+{
+    public int IdUnidad { get; set; }
+
+    public string? NombreCorto { get; set; }
+
+    public string? NombreLargo { get; set; }
+
+    public virtual ICollection<RecetaIngrediente> RecetaIngredientes { get; set; } = new List<RecetaIngrediente>();
+}
